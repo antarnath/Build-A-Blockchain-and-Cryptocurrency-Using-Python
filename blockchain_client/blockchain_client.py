567c7108e53cb4908b88c067873c7fd7c581f0c2
antarnath <antar.ch.nath@gmail.com>
2024-10-06 07:37:52 +0600

    create public and private key

--- blockchain_client/blockchain_client.py
+++ blockchain_client/blockchain_client.py
@@ -1,4 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+import Crypto 
+import Crypto.Random
+from Crypto.PublicKey import RSA
+import binascii
 
 class Transaction:
   def __init__(self, sender_address, sender_private_key, recipient_address, value):
@@ -21,6 +25,22 @@ def make_transaction():
 @app.route('/view/transactions') 
 def view_transactions():
   return render_template('./view_transaction.html')
+
+
+@app.route('/wallet/new')
+def new_wallet():
+  random_gen = Crypto.Random.new().read
+  private_key = RSA.generate(1024, random_gen)
+  public_key = private_key.publickey()
+  
+  private_key_hex = binascii.hexlify(private_key.export_key(format='DER')).decode('ascii')
+  public_key_hex = binascii.hexlify(public_key.export_key(format='DER')).decode('ascii')
+  response = {
+    'private_key': private_key_hex,
+    'public_key': public_key_hex
+  }
+
+  return jsonify(response), 200
 
 
 if __name__ == '__main__':
